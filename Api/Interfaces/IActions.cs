0554d71017aa431e56e3b600948d23ce38473831
AndroidNumber17 <john@functional.software>
2023-03-25 14:34:49 -0600

    DI Configgurations

---
+++ Api/Interfaces/IActions.cs
@@ -0,0 +1,18 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Interfaces
+{
+    public interface IActions
+    {
+        Task<IEnumerable<Poster>> GetPostersAsync();
+        Task<Poster> GetPosterAsync(string id);
+        Task CreatePosterAsync(Poster poster);
+        Task DeletePosterAsync(string id);
+        Task UpdatePosterAsync(Poster poster);
+    }
+}
---
+++ Api/Models/Poster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Poster
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Band { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
